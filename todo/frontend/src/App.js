4394f2c3e3c673a48de6a7547b2a6d1ea8c07b8d
Phillip Douglas <pdouglasjr@proton.me>
2023-08-05 23:52:01 -0400

    finished todo app

--- todo/frontend/src/App.js
+++ todo/frontend/src/App.js
@@ -1,40 +1,38 @@
 import React, { Component } from 'react';
-
-const list = [
-  {
-    "id": 1,
-    "title": "Learn HTTP",
-    "body": "I need to learn HTTP! All of it!"
-  },
-  {
-    "id": 2,
-    "title": "Second Item",
-    "body": "Learn Django REST Framework"
-  },
-  {
-    "id": 3,
-    "title": "Third Item",
-    "body": "Last todo. I don't know. Bleh."
-  }
-]
+import axios from 'axios';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { list };
+  state = {
+    todos: []
+  };
+
+  componentDidMount() {
+    this.getTodos();
+  }
+
+  getTodos() {
+    axios
+      .get('http://127.0.0.1:8000/api/')
+      .then(res => {
+        console.log(res)
+        this.setState({ todos: res.data })
+      })
+      .catch(err => {
+        console.log(err);
+      })
   }
 
   render() {
     return (
       <div>
-        {this.state.list.map(item => (
+        {this.state.todos.map(item => (
           <div key={item.id}>
             <h1>{item.title}</h1>
             <p>{item.body}</p>
           </div>
         ))}
       </div>
-    )
+    );
   }
 }
 
