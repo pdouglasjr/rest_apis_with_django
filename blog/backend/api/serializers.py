5b08041e7ff2972d164879b6c21c798aa9785867
Phillip Douglas <pdouglasjr@proton.me>
2023-08-15 00:39:52 -0400

    updates

--- blog/backend/api/serializers.py
+++ blog/backend/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+
 from posts.models import Post
 
 
@@ -7,3 +8,9 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         fields = ("id", "author", "title", "body", "created_at", "updated_at")
         model = Post
+
+    def to_representation(self, instance):
+        representation = super(PostSerializer, self).to_representation(instance)
+        representation["author"] = instance.author.username
+
+        return representation
--- blog/backend/api/views.py
+++ blog/backend/api/views.py
@@ -1,14 +1,22 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 from posts.models import Post
 from .serializers import PostSerializer
 
+from .permissions import IsAuthorOrReadOnly
+
 
 class PostList(generics.ListCreateAPIView):
+    # permission_classes = (permissions.IsAuthenticated,) # OOB view-level permission
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    # permission_classes = (permissions.IsAuthenticated,) # OOB view-level permission
+
+    permission_classes = (
+        IsAuthorOrReadOnly,
+    )  # custom view-level, object-level permission
     queryset = Post.objects.all()
     serializer_class = PostSerializer
